d2a47776f98103cd19b4371f8821906abf9d3a35
marim <mariamemampp00@gmail.com>
2026-05-10 18:19:22 +0300

    make custom js file for the tasks

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,1 @@
-window.taskApp = () => {
-    return {
-        tasks: [],
-        newTask: '',
-        submit() {
-            this.tasks.push({ body: this.newTask, completed: false });
-            this.newTask = '';
-        }
-    };
-}
+import './components/TaskApp.js';
---
+++ resources/js/components/TaskApp.js
@@ -0,0 +1,10 @@
+window.taskApp = () => {
+    return {
+        tasks: [],
+        newTask: '',
+        submit() {
+            this.tasks.push({ body: this.newTask, completed: false });
+            this.newTask = '';
+        }
+    };
+}
