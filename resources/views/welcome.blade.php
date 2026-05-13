1e0e4fe9feabe1e7c28700f640bdc621fe1ba439
marim <mariamemampp00@gmail.com>
2026-05-13 15:36:05 +0300

    DOM event like mouseover  => if i mouseover it i get alert

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -25,23 +25,10 @@
         <link href="https://cdn.jsdelivr.net/npm/tailwindcss@3/dist/tailwind.min.css" rel="stylesheet">
         <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     </head>
-    <body class="grid items-center justify-center h-screen">
-    <div x-data="{show: false}" @click.away="show = false">
-           <button @click="show = ! show">Links</button>
-
-        <div class=" absolute bg-black text-white py-2 rounded mt-1"
-             x-show="show"
-             x-transition:enter="transition duration-200 transform ease-out"
-             x-transition:enter-start="scale-75"
-             x-transition:leave="transition duration-100 transform ease-in"
-             x-transition:leave-end="opacity-0 scale-90"
-
-        >
-            <a class= "block hover:bg-gray-800 text-xs py-px px-4" href="#">Edit</a>
-            <a class= "block hover:bg-gray-800 text-xs py-px px-4" href="#">Delete</a>
-            <a class= "block hover:bg-gray-800 text-xs py-px px-4" href="#">Report Spam</a>
-        </div>
-    </div>
+    <body class="p-12">
+     <div x-data>
+         <button @mouseover="alert('I was clicked')">Click</button>
+     </div>
     </body>
 
 
